060e6c370ddd15d8e6f1814b5aff85c83d66ee94
Angkush <angkysahu2502@gmail.com>
2022-08-18 03:04:20 +0530

    removed all js files and updated package.json

--- client/src/components/loading.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Loading = () => {
-    return (<span className="h-16 w-16 block rounded-full border-4 border-t-tertiary animate-spin"></span>);
-};
-exports.default = Loading;
